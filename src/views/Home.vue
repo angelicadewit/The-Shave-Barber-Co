d72830da230163709207e6f280cc5f8a7f1876eb
angelicadewit <32370846+angelicadewit@users.noreply.github.com>
2019-02-26 02:05:15 -0500

    added home css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,13 @@
   <div class="homepage">
     <div class="hero">
       <h2>
-        <span>Traditional</span>
-        <span>Barber</span>
-        <span>Shop</span>
+        <div class="top">
+          <span>Traditional</span>
+        </div>
+        <div class="bottom">
+          <span>Barber</span>
+          <span>Shop</span>
+        </div>
       </h2>
       <img src="../assets/photo.png" alt="">
     </div>
@@ -15,29 +19,22 @@
 
 
 <style scoped lang="scss">
-// .homepage{
-  // position: absolute;
-	// margin: auto;
-	// top: 0; 
-	// left: 0; 
-	// bottom: 0; 
-	// right: 0;
-
-	// height: 60vw;
-	// background-color: rgba(0, 0, 0, 0.0);
-
-// }
 
 .hero{
-	margin: 0% auto;
+	margin: 3% auto;
 	display: flex;
   justify-content: center;
   background: url(../assets/demask-background.png) no-repeat;
   background-size: contain;
   padding: 50px;
   position: relative;
-  width: 60vw;
+  width: 70vw;
   height: 40vw;
+
+  @include tablet{
+    width: 60vw;
+    height: 40vw;
+  }
 
   img{
     border: 30px solid black;
@@ -47,7 +44,45 @@
 
   h2{
     color: white;
+    position: relative;
+    font-size: 1rem;
+    text-transform: uppercase;
+    letter-spacing: 1rem;
+    margin-block-start: 0.0em;
+    margin-block-end: 0.0em;
+    text-align: center;
+    
+
+    @include tablet{
+      font-size: 2.3rem;
+      display: flex;
+      justify-content: center;
+    }
+  
+    span{
+        display: block;
+    }
+  }
+
+  .top,
+  .bottom{
     position: absolute;
+    margin: 2%;
+  }
+  
+  .top{
+    left: 0;
+  }
+
+  .bottom{
+    bottom: 0;
+    left: 50%;
+    transform: translateX(14%);
+    margin: 0 auto;
+
+    span{
+      letter-spacing: 2rem;
+    }
   }
 }
 </style>
