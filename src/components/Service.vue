30e683a5f649bc4459f32a6401c64d80987172d2
angelicadewit <32370846+angelicadewit@users.noreply.github.com>
2019-03-06 10:45:06 -0500

    added more styling in service

---
+++ src/components/Service.vue
@@ -0,0 +1,83 @@
+<template>
+    <li class="service">
+        <div>
+            <h3>{{ barberCoService.name }}</h3>
+            <!-- <img :src="require(`@/assets/${valuedTeamMemberData.name}.png`)" alt=""> -->
+            <p class="price-span">
+                <span class="price">${{ barberCoService.price }}</span>
+                <span class="book">Book</span>
+            </p>
+        </div>
+        <div class="description-text">
+            <p class="bio-text">
+                {{ barberCoService.description }}
+            </p>
+        </div>
+    </li>
+</template> 
+
+<script>
+export default {
+    name: 'service',
+    props: {
+        barberCoService: Object,
+        active: false,
+    },
+    methods: {
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+.service{
+    font-size: 2.2rem;
+
+    h3{
+        color: #906337;
+        text-transform: uppercase;
+    }
+
+    .price-span{
+        color: #906337;
+        background: url("../assets/beigespacer.png") no-repeat;
+        background-size: contain;
+
+        @include desktop {
+            .price {
+                display: inline;
+            }
+            .book {
+                display: none;
+            }
+        }
+        span {
+            background-color: white;
+            padding: 0 5px;
+            @include desktop {
+                padding: 0 10px;
+
+            }
+        }
+        
+    }
+
+    @include desktop {
+        &:hover {
+            .price-span{
+                .price {
+                    display: none;
+                }
+                .book {
+                    display: inline;
+                }
+            }
+        }
+    }
+
+    // .book-span{
+    //     color: $header-teal;
+    // }
+}
+</style>
+
--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,66 +1,75 @@
 <template>
-	<div class="services">
-		Services
-	<ul class="thumbnails">
-		<div v-for="staffMember in staff" :key="staffMember.id" @click="currentBarber = staffMember">
-		<barber :key="staffMember.id" :class="[staffMember.class]" :valuedTeamMemberData="staffMember"></barber>
-		</div>
-	</ul>
+    <div class="services">
+        <ul class="services">
+            <service v-for="service in services" :key="service.id" :barberCoService="service"></service>
+        </ul>
+    </div>
 </template>
 
 <script>
-import barber from '@/components/Service.vue'
+import service from '@/components/Service.vue'
 
 export default {
-	name: 'services',
-	components: {
-	},
-	data: function() {
-	return {
-		services: [
-		{
-			id: 0,
-			name: "Men's Haircut",
-			price: "35",
-			description: "A standard cut for a shorter, traditional men's style employing the use of shears or clippers as the stylist sees fit, then cleaning the neck line with clippers.",
-		},
-		{
-			id: 1,
-			name: "Long Haircut",
-			price: "40",
-			description: "A cut for longer hair, often layered or texturized and may require specialized shear work.",
-		},
-		{
-			id: 2,
-			name: "Buzz Cut",
-			price: "20",
-			description: "A clipper cut across the entire head using only one guard. No fading.",
-		},
-		{
-			id: 3,
-			name: "Specialty Cut",
-			price: "30",
-			description: "You will experience the artistic talents of our barbers and stylists when they use their skills to meet your needs, whether it is a flat top, skin fade or the like.",
-		},
-		{
-			id: 4,
-			name: "Beard Trim",
-			price: "15",
-			description: "Using clippers or shears to trim or shape facial hair, followed by a razor line up of the jaw, neck and cheeks. Finished with a relaxing hot towel.",
-		},
-		{
-			id: 5,
-			name: "Hot Shave",
-			price: "20",
-			description: "Using clippers or shears to trim or shape facial hair, followed by a razor line up of the jaw, neck and cheeks. Finished with a relaxing hot towel.",
-		},
-		]
-	}
-	}
+    name: 'services',
+    components: {
+        service
+    },
+    data: function() {
+    return {
+        services: [
+                {
+                    id: 0,
+                    name: "Men's Haircut",
+                    price: "35",
+                    description: "A standard cut for a shorter, traditional men's style employing the use of shears or clippers as the stylist sees fit, then cleaning the neck line with clippers.",
+                },
+                {
+                    id: 1,
+                    name: "Long Haircut",
+                    price: "40",
+                    description: "A cut for longer hair, often layered or texturized and may require specialized shear work.",
+                },
+                {
+                    id: 2,
+                    name: "Buzz Cut",
+                    price: "20",
+                    description: "A clipper cut across the entire head using only one guard. No fading.",
+                },
+                {
+                    id: 3,
+                    name: "Specialty Cut",
+                    price: "30",
+                    description: "You will experience the artistic talents of our barbers and stylists when they use their skills to meet your needs, whether it is a flat top, skin fade or the like.",
+                },
+                {
+                    id: 4,
+                    name: "Beard Trim",
+                    price: "15",
+                    description: "Using clippers or shears to trim or shape facial hair, followed by a razor line up of the jaw, neck and cheeks. Finished with a relaxing hot towel.",
+                },
+                {
+                    id: 5,
+                    name: "Hot Shave",
+                    price: "20",
+                    description: "Using clippers or shears to trim or shape facial hair, followed by a razor line up of the jaw, neck and cheeks. Finished with a relaxing hot towel.",
+                },
+            ],
+        }
+    }
+
 }
 </script>
 
 <style scoped lang="scss">
-
+.services{
+    margin: auto;
+    width: 70vw;
+    max-width: 800px;
+    background: white url("../assets/barberchair.png") no-repeat 50%;
+}
+ul {
+    margin: 10% auto;
+    margin-bottom: 0%;
+}
 
 </style>
